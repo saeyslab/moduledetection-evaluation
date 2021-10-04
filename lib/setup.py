589ddaadf6056e08461c95c0ca077d0ae7113b5b
Wouter Saelens <wouter.saelens@gmail.com>
2021-10-04 12:56:25 +0200

    update setup to new cython

--- lib/setup.py
+++ lib/setup.py
@@ -1,17 +1,21 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
   name = 'ebcubed',
   ext_modules = cythonize("ebcubed.pyx"),
+  include_dirs=[numpy.get_include()]
 )
 
 setup(
   name = 'jaccard',
   ext_modules = cythonize("jaccard.pyx"),
+  include_dirs=[numpy.get_include()]
 )
 
 setup(
   name = 'cfisher',
   ext_modules = cythonize("cfisher.pyx"),
+  include_dirs=[numpy.get_include()]
 )
